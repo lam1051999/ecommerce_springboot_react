a1507aba20128d010fa9d3331900facedef023fa
lam1051999 <lam1051999.com>
2023-05-12 19:29:19 +0700

    dockerize our application

---
+++ ec2_command.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+yum update -y
+
+# Install Docker
+amazon-linux-extras install docker -y
+service docker start
+systemctl enable docker
+usermod -a -G docker ec2-user
+chmod 666 /var/run/docker.sock
+
+# Install Docker Compose
+curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Install git in your EC2 instance
+yum install git -y
+
+# Clone and run the application
+cd ~
+git clone https://github.com/lam1051999/ecommerce_springboot_react.git
+cd ecommerce_springboot_react
