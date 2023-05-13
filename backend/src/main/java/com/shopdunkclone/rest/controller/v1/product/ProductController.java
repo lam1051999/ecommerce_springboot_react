d1224a9d0c34684cb66413cf1227aa0cdc3b4ad9
lam1051999 <lam1051999.com>
2023-05-13 18:56:08 +0700

    dockerize our application

--- backend/src/main/java/com/shopdunkclone/rest/controller/v1/product/ProductController.java
+++ backend/src/main/java/com/shopdunkclone/rest/controller/v1/product/ProductController.java
@@ -13,8 +13,6 @@ import com.shopdunkclone.rest.service.product.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- script/build.sh
+++ script/build.sh
@@ -10,14 +10,14 @@ else
     exit -1
 fi
 
-docker build -f Dockerfile -t shopdunk_backend --target backend .
-docker build -f Dockerfile -t shopdunk_nginx --target nginx .
+docker build -f Dockerfile -t ${DOCKER_USERNAME}/${DOCKER_IMAGE_BACKEND} --target backend .
+docker build -f Dockerfile -t ${DOCKER_USERNAME}/${DOCKER_IMAGE_NGINX} --target nginx .
 
 cat ./script/docker_password.txt | docker login ${DOCKER_REGISTRY} -u ${DOCKER_USERNAME} --password-stdin
 
 if [ ${TARGET_ENV} == "production" ]; then
-    docker tag ${DOCKER_IMAGE_BACKEND}:${DOCKER_TAG} ${DOCKER_REGISTRY}/${DOCKER_USERNAME}/${DOCKER_IMAGE_BACKEND}:${DOCKER_TAG}
-    docker tag ${DOCKER_IMAGE_NGINX}:${DOCKER_TAG} ${DOCKER_REGISTRY}/${DOCKER_USERNAME}/${DOCKER_IMAGE_NGINX}:${DOCKER_TAG}
+    docker tag ${DOCKER_USERNAME}/${DOCKER_IMAGE_BACKEND}:${DOCKER_TAG} ${DOCKER_REGISTRY}/${DOCKER_USERNAME}/${DOCKER_IMAGE_BACKEND}:${DOCKER_TAG}
+    docker tag ${DOCKER_USERNAME}/${DOCKER_IMAGE_NGINX}:${DOCKER_TAG} ${DOCKER_REGISTRY}/${DOCKER_USERNAME}/${DOCKER_IMAGE_NGINX}:${DOCKER_TAG}
     docker push ${DOCKER_REGISTRY}/${DOCKER_USERNAME}/${DOCKER_IMAGE_BACKEND}:${DOCKER_TAG}
     docker push ${DOCKER_REGISTRY}/${DOCKER_USERNAME}/${DOCKER_IMAGE_NGINX}:${DOCKER_TAG}
 fi
